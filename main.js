9faa54eb3bbc480b83802fa6bd071f13ff679498
AnikHaque <ekramul15-811@diu.edu.bd>
2023-08-04 11:19:07 +0600

    added

---
+++ main.js
@@ -0,0 +1,24 @@
+void main() {
+  const int a = 7;
+  const int b = 3;
+
+  // Addition
+  int addition = a + b;
+  print("Addition: $a + $b = $addition");
+
+  // Subtraction
+  int subtraction = a - b;
+  print("Subtraction: $a - $b = $subtraction");
+
+  // Multiplication
+  int multiplication = a * b;
+  print("Multiplication: $a * $b = $multiplication");
+
+  // Division
+  double division = a / b;
+  print("Division: $a / $b = $division");
+
+  // Modulus
+  int modulus = a % b;
+  print("Modulus: $a % $b = $modulus");
+}
